c5aaeb2d495376c440dd1c52a7a5b13089fb78a9
zph <zphhpzzph@gmail.com>
2026-05-15 00:49:56 +0800

    配置axios，并在vite.config.ts中dev server的代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { loadEnv } from 'vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue()
-  ]
+export default defineConfig(({ mode }) => {
+  const randomString = '/asdfasdf'
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue()
+    ],
+    server: {
+      proxy: {
+        [randomString]: {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(randomString, '')
+        }
+      }
+    }
+  }
 })
