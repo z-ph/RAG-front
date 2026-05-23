4bad3053621787a64e3174e7c64372a0efa1bd9c
wan <wan@example.com>
2026-05-23 16:06:52 +0800

    恢复 apiClient.ts 至 4c34702 之前版本

--- src/core/apiClient.ts
+++ src/core/apiClient.ts
@@ -1,23 +1,23 @@
-import axios from 'axios'
-import { apiClientConfig } from './config'
-import { getToken } from '../utils/token'
-const apiClient = axios.create(apiClientConfig)
+import axios from "axios";
+import { apiClientConfig } from "./config";
+import { getToken } from "../utils/token";
+const apiClient = axios.create(apiClientConfig);
 
 apiClient.interceptors.request.use((config) => {
   const token = getToken()
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers["Authorization"] = `Bearer ${token}`;
   }
-  return config
-})
+  return config;
+});
 
 apiClient.interceptors.response.use((response) => {
   if (response.data.code != 1) {
-    throw new Error(response.data.msg || 'Unknown error')
+    throw new Error(response.data.msg || "Unknown error");
   }
-  return response
+  return response;
 }, (error) => {
-  return Promise.reject(error)
-})
+  return Promise.reject(error);
+});
 
 export default apiClient
